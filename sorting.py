58c212c9ed50c78289db999c14e0e5829a2e2d4e
Gloria <gchoi@cmc.edu>
2020-03-27 15:38:35 -0700

    Update README and finish homework

--- sorting.py
+++ sorting.py
@@ -47,6 +47,30 @@ def _merged(xs, ys, cmp=cmp_standard):
     Runs in linear time.
     '''
 
+    newlist = []
+    i = 0
+    j = 0
+       
+    while i<len(xs) and j<len(ys):
+        if cmp==cmp_standard:
+            if cmp(xs[i],ys[j]) <= 0:
+                newlist.append(xs[i])
+                i+=1
+            else:  #when cmp == 1
+                newlist.append(ys[j])
+                j+=1
+        else: # when cmp = cmp_reverse
+            if cmp(xs[i],ys[j]) <= 0:
+                newlist.append(xs[i])
+                i+=1
+            else:  #when cmp == 1
+                newlist.append(ys[j])
+                j+=1
+    return newlist + xs[i:] + ys[j:]
+ 
+
+
+
 
 def merge_sorted(xs, cmp=cmp_standard):
     '''
@@ -63,6 +87,14 @@ def merge_sorted(xs, cmp=cmp_standard):
 
     You should return a sorted version of the input list xs
     '''
+    if len(xs) <= 1:
+        return xs
+    else:
+        left = xs[0:len(xs)//2]
+        right = xs[len(xs)//2:]
+        sorted_left = merge_sorted(left, cmp) # sort the left
+        sorted_right = merge_sorted(right, cmp) # sort the right
+        return _merged(sorted_left, sorted_right, cmp)
 
 
 def quick_sorted(xs, cmp=cmp_standard):
@@ -86,7 +118,29 @@ def quick_sorted(xs, cmp=cmp_standard):
 
     You should return a sorted version of the input list xs
     '''
+    list_less = []
+    list_greater = []
+    list_equal = []
+   
 
+    if len(xs) <= 1:
+        return xs
+    else:
+        p = xs[0]
+        for x in xs:
+            if x<p:
+                list_less.append(x)
+            elif x>p:
+                list_greater.append(x)
+            else: # x == p
+                list_equal.append(x)
+        sorted_less = quick_sorted(list_less, cmp)
+        sorted_greater = quick_sorted(list_greater, cmp)
+        
+        if cmp==cmp_standard:
+            return sorted_less + list_equal + sorted_greater
+        else: # if cmp=cmp_reverse
+            return sorted_greater + list_equal + sorted_less
 
 def quick_sort(xs, cmp=cmp_standard):
     '''
@@ -100,3 +154,23 @@ def quick_sort(xs, cmp=cmp_standard):
     to implement quick_sort as an in-place algorithm.
     You should directly modify the input xs variable instead of returning a copy of the list.
     '''
+    if len(xs) <= 1:
+        return xs
+    def helper(xs,lo,hi):
+        if lo<hi:
+            p = partition(xs,lo,hi,cmp)
+            helper(xs,lo,p-1)
+            helper(xs,p+1,hi)
+            return xs
+    return helper(xs,0,len(xs)-1)
+
+def partition(xs,lo,hi,cmp=cmp_standard):
+    pivot = xs[hi]
+    i = lo
+
+    for j in range(lo,hi):
+        if (xs[j] < pivot and cmp == cmp_standard) or (xs[j] > pivot and cmp==cmp_reverse):
+            xs[i], xs[j] = xs[j], xs[i]
+            i += 1
+    xs[i],xs[hi] = xs[hi],xs[i]
+    return i
